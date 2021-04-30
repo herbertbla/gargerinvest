045e60a38b0f9ae50910a03a48cbab23c4ce7c7a
herbertbla <jupiter@gmx.at>
2021-04-30 21:18:02 +0200

    Add files via upload
    claudia

---
+++ src/app/invest-grund/invest-grund.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'invest-grund',
+  templateUrl: './invest-grund.component.html',
+  styleUrls: ['./invest-grund.component.less']
+})
+export class InvestGrundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
